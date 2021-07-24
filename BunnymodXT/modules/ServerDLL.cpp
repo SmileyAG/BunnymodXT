1fdfe68e54d8c6acb517795f691753f52e19c468
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2021-07-24 20:24:07 +0400

    Added ORIG_CChangeLevel__ChangeLevelNow_Linux in REG check

--- BunnymodXT/modules/ServerDLL.cpp
+++ BunnymodXT/modules/ServerDLL.cpp
@@ -1117,7 +1117,7 @@ void ServerDLL::RegisterCVarsAndCommands()
 	}
 	if (ORIG_CTriggerSave__SaveTouch)
 		REG(bxt_disable_autosave);
-	if (ORIG_CChangeLevel__ChangeLevelNow)
+	if (ORIG_CChangeLevel__ChangeLevelNow || ORIG_CChangeLevel__ChangeLevelNow_Linux)
 		REG(bxt_disable_changelevel);
 	#undef REG
 }
