243eb48256c3f620e46e6a68f14d8a80832a4951
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2024-07-15 01:00:05 +0400

    databank: added get_typedescription_fieldtype function to determine FIELDTYPE from TYPEDESCRIPTION

--- BunnymodXT/databank.hpp
+++ BunnymodXT/databank.hpp
@@ -9,4 +9,5 @@ namespace databank
 {
 	std::string get_difficulty(const int skill);
 	std::string get_hitgroup(const int iHitgroup);
+	std::string get_typedescription_fieldtype(const int fieldType);
 }
